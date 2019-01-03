ce2794297292081ada161208b92344b69a9897de
PlaxBOTS <45571960+PlaxBOTS@users.noreply.github.com>
2019-01-03 19:06:30 +0200

    Update oylama.js

--- komutlar/oylama.js
+++ komutlar/oylama.js
@@ -1,25 +1,46 @@
 const Discord = require('discord.js');
 
-exports.run = (client, message, args) => {
-	let mesaj = args.slice(0).join(' ');
-	if (mesaj.length < 1) return message.reply('Yapıcağın Oylamanın İsmin Yaz');
-    message.delete();
-    const embed = new Discord.RichEmbed()
-    .setAuthor('OYLAMA')
-    .setColor(3447003)
-    .setDescription(`${mesaj} \n\n\ Evet İçin: :thumbsup: Hayır İçin: :thumbsdown: `)
-    return message.channel.sendEmbed(embed);
-};
+ exports.run = (client, message, args) => {
+   message.delete();
 
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
+   let question = args.join(' ');
+
+   let user = message.author.username
+
+   if (!question) return message.channel.sendEmbed(
+
+     new Discord.RichEmbed()
+
+     .addField(`:no_entry_sign:  Oylama için ne yazmam gerektiğini yazmalısın :no_entry_sign: `)).then(m => m.delete(5000));
+
+     message.channel.sendEmbed(
+
+       new Discord.RichEmbed()
+
+       .setColor("RANDOM")
+       .setThumbnail(client.user.avatarURL)
+       .setTimestamp()
+
+       .addField(`**Oylama**`, `**${question}**`)).then(function(message) {
+
+         message.react('✅');
+
+         message.react('❌');
+
+       });
+
+     };
+
+     exports.conf = {
+       enabled: true,
+       guildOnly: false,
+       aliases: ['oylama'],
+	     
   permLevel: 2
 };
 
 exports.help = {
   name: 'oylama',
-  description: 'Oylama Yapar.',
-  usage: 'oylama'
+  description: 'Oylama yapmanızı sağlar.',
+  usage: 'oylama <oylamaismi>'
 };
