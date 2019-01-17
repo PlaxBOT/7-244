0dd152320f37a46c896aa198ff631ace148b40c2
PlaxBOTS <45571960+PlaxBOTS@users.noreply.github.com>
2019-01-17 19:30:55 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,10 +6,27 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Tüm komutlar çalıştırılıyor!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı! BOT aktif!`);
   client.user.setStatus("online");
-  client.user.setGame(`..yardım|Türkiye`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
-};
+   var oyun = [
+        "Prefiximiz : p!",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        `${client.users.size} kullanıcıya hizmet veriyorum.`,
+        `${client.guilds.size} sunucuya hizmet veriyorum.`
+
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setGame(oyun[random], "https://www.twitch.tv/joeartv");
+        }, 2 * 2500);
+}
